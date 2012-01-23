4c1da66881ddb75c89e5530cbe89205d2ba50323
anshul.kundaje@gmail.com <anshul.kundaje@gmail.com@e90616db-66ff-9a0a-f173-6d42999a5b6a>
2012-01-23 11:09:51 +0000

    Added support for null models in interaction strength and pairwise interaction computation

--- trunk/scripts/temp.R
+++ trunk/scripts/temp.R
@@ -110,9 +110,10 @@ rfhome <- initialize.rulefit(rf.package.path='/media/fusion10/work/encode/learni
 #   }
 # }
 
-for (cell in c("K562")) {
-  input.dir <- sprintf("/media/fusion10/work/encode/learning/combinatorics/results/TFCentric/randneg_iter/%s/average/OA_max_tree6_average",cell)
-  output.dir <- sprintf("/media/fusion10/work/encode/learning/combinatorics/figures/TFCentric/randneg_iter/%s/OA_max_tree6_average",cell)
+for (cell in c("GM12878","H1hesc","Hepg2","Hela-S3")) {
+# for (cell in c("K562")) {  
+  input.dir <- sprintf("/media/fusion10/work/encode/learning/combinatorics/results/TFCentric/randneg_iter/%s/average/null_append_OA_max_tree6_average",cell)
+  output.dir <- sprintf("/media/fusion10/work/encode/learning/combinatorics/figures/TFCentric/randneg_iter/%s/null_append_OA_max_tree6_average",cell)
   dir.create(path=output.dir,recursive=T)
   for (i in list.files(path=input.dir,pattern=".*Rdata$",full.names=T)) {
     tryCatch( plot.average.vi(rulefit.results=i,output.dir=output.dir,thresh=5), error = function(e) cat("ERROR\n"))
